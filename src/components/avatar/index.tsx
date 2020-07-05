382e1956d6229a8aa6e246eb2f4b318002fb1c84
gu <1982775886@qq.com>
2020-07-05 15:18:11 +0800

    dart-sass

---
+++ src/components/avatar/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './index.mod.scss';
+
+export default class XAvatar extends React.Component {
+  render() {
+    return (
+      <div className="avatar">
+        头像
+      </div>
+    );
+  }
+}
